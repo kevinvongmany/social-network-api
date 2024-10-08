17c28fdfde4a1d9231f88f7ab6d507ce941fdf7e
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-08 23:40:58 +1100

    Updated thought routes to delete reaction by id

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -19,9 +19,14 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
 router
   .route('/:thoughtId/reactions')
   .post(addReaction)
+  .delete(removeReaction);
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router
+  .route('/:thoughtId/reactions/:reactionId')
   .delete(removeReaction);
 
 module.exports = router;
