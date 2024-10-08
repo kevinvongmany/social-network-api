35cc15c061afa98904febaf974d33bfd84a8bf75
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-08 19:42:51 +1100

    Fixed user model to only populate thought _ids

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const { thoughtSchema } = require('./Thought');
 
 // Schema to create User model
 const userSchema = new Schema(
@@ -16,7 +15,12 @@ const userSchema = new Schema(
       unique: true,
       match: [/.+@.+\..+/, 'Must match an email address!'],
     },
-    thoughts: [thoughtSchema],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
     friends: [
       {
         type: Schema.Types.ObjectId,
