173f2deba3e291b13b845eaedddcf1fd4d3246a1
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-08 19:06:23 +1100

    Added routes for reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,8 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  removeReaction,
 } = require('../../controllers/thoughtController.js');
 
 // /api/thoughts
@@ -16,5 +18,10 @@ router
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(removeReaction);
 
 module.exports = router;
